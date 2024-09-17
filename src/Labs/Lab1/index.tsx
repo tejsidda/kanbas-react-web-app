722af7e9339e380100611f59c007889057a3a583
tejsidda <tejsidda34@gmail.com>
2024-09-17 16:18:32 -0400

    try2

--- src/Labs/Lab1/index.tsx
+++ src/Labs/Lab1/index.tsx
@@ -111,18 +111,18 @@ Your favorite books (in no particular order)
       </div>
 
 
-
-  <div id="wd-images">
+      <div id="wd-images">
   <h4>Image tag</h4>
   Loading an image from the internet:
   <br />
   <img id="wd-starship"
     width="400px"
-   src="https://www.staradvertiser.com/wp-content/uploads/2021/08/web1_Starship-gap2.jpg"
+    src="https://www.staradvertiser.com/wp-content/uploads/2021/08/web1_Starship-gap2.jpg"
+    alt="Image of Starship with a gap" 
   />
   <br />
-
 </div>
+
 
 
 
