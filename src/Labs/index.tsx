8badc17cdcba8e4e3b37876714cafdb90b6c2859
tejsidda <tejsidda34@gmail.com>
2024-09-17 07:16:18 -0400

    trai

--- src/Labs/index.tsx
+++ src/Labs/index.tsx
@@ -1,13 +1,8 @@
 import Lab1 from "./Lab1";
-
-export default function Labs(){
-    return(
-    <div>
-        <h1>
-             Labs
-        </h1>
-        <Lab1 />
-        
+export default function Labs() {
+  return (
+    <div id="wd-labs">
+      <h1>Labs</h1>
+      <Lab1 />
     </div>
-);
-}
+);}
