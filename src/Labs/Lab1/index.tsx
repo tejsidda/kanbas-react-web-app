cef72298951d685f1e5c4f12bfc05af51af127b1
tejsidda <tejsidda34@gmail.com>
2024-09-17 05:42:25 -0400

    next

---
+++ src/Labs/Lab1/index.tsx
@@ -0,0 +1,9 @@
+export default function Labs1(){
+    return(
+    <div>
+        <h1>
+            Labs1
+        </h1>
+
+    </div> );
+}
---
+++ src/Labs/index.tsx
@@ -0,0 +1,13 @@
+import Lab1 from "./Lab1";
+
+export default function Labs(){
+    return(
+    <div>
+        <h1>
+             Labs
+        </h1>
+        <Lab1 />
+        
+    </div>
+);
+}
