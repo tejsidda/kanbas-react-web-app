2bc0a1da3a2e3d20c589ff3942c566858f88f924
tejsidda <tejsidda34@gmail.com>
2024-09-17 07:07:59 -0400

    tags

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,11 @@
 import React from 'react';
-import './App.css';
-import './Labs';
 import Labs from './Labs';
 
 function App() {
   return (
     <div>
-      
-        <h1>
-         Welcome to Kanbas 
-        </h1>
-        <Labs />
-      
-    </div>
+      <Labs />
+      </div>
   );
 }
 
