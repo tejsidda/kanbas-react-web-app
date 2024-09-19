4f2e13cf10accb83ea62916b1be29949f73640cb
tejsidda <tejsidda34@gmail.com>
2024-09-19 19:56:37 -0400

    k

--- src/Labs/TOC.tsx
+++ src/Labs/TOC.tsx
@@ -7,7 +7,7 @@ export default function TOC() {
         <li><Link to="/Labs/Lab2">Lab 2</Link></li>
         <li><Link to="/Labs/Lab3">Lab 3</Link></li>
         <li><Link to="/Kanbas">Kanbas</Link></li>
-        <a id="wd-github" href="https://github.com/tejsidda/kanbas-react-web-app">Github link</a>
+        <li><a id="wd-github" href="https://github.com/tejsidda/kanbas-react-web-app">Github link</a></li>
       </ul>
     );
   }
