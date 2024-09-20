a8fd1a980c54cdc13b755b33a8c044b82bea3d7d
tejsidda <tejsidda34@gmail.com>
2024-09-19 20:19:51 -0400

    s

--- src/Labs/Lab1/index.tsx
+++ src/Labs/Lab1/index.tsx
@@ -60,9 +60,11 @@ My favorite books (in no particular order)
   <li>Red Mars</li>
   <li>The Forever War</li>
 </ul>
-Your favorite books (in no particular order)
+Your favorite books
 <ul id="wd-your-books">
-  {/* complete on your own */}
+  <li>Mahabarratha</li>
+  <li>The Killing Joke</li>
+  <li>Ramayana</li>
 </ul>
 
 
@@ -71,44 +73,87 @@ Your favorite books (in no particular order)
 
 
 <div id="wd-tables">
-        <h4>Table Tag</h4>
-        <table border={1} width="100%">
-          <thead>
-            <tr>
-              <th>Quiz</th>
-              <th>Topic</th>
-              <th>Date</th>
-              <th>Grade</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>Q1</td>
-              <td>HTML</td>
-              <td>2/3/21</td>
-              <td>85</td>
-            </tr>
-            <tr>
-              <td>Q2</td>
-              <td>CSS</td>
-              <td>2/10/21</td>
-              <td>90</td>
-            </tr>
-            <tr>
-              <td>Q3</td>
-              <td>JavaScript</td>
-              <td>2/17/21</td>
-              <td>95</td>
-            </tr>
-          </tbody>
-          <tfoot>
-            <tr>
-              <td colSpan={3}>Average</td>
-              <td>90</td>
-            </tr>
-          </tfoot>
-        </table>
-      </div>
+  <h4>Table Tag</h4>
+  <table border={1} width="100%">
+    <thead>
+      <tr>
+        <th>Quiz</th>
+        <th>Topic</th>
+        <th>Date</th>
+        <th>Grade</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>Q1</td>
+        <td>HTML</td>
+        <td>2/3/21</td>
+        <td>85</td>
+      </tr>
+      <tr>
+        <td>Q2</td>
+        <td>CSS</td>
+        <td>2/10/21</td>
+        <td>90</td>
+      </tr>
+      <tr>
+        <td>Q3</td>
+        <td>JavaScript</td>
+        <td>2/17/21</td>
+        <td>95</td>
+      </tr>
+      <tr>
+        <td>Q4</td>
+        <td>React</td>
+        <td>2/24/21</td>
+        <td>88</td>
+      </tr>
+      <tr>
+        <td>Q5</td>
+        <td>Node.js</td>
+        <td>3/3/21</td>
+        <td>92</td>
+      </tr>
+      <tr>
+        <td>Q6</td>
+        <td>Express.js</td>
+        <td>3/10/21</td>
+        <td>87</td>
+      </tr>
+      <tr>
+        <td>Q7</td>
+        <td>MongoDB</td>
+        <td>3/17/21</td>
+        <td>93</td>
+      </tr>
+      <tr>
+        <td>Q8</td>
+        <td>TypeScript</td>
+        <td>3/24/21</td>
+        <td>91</td>
+      </tr>
+      <tr>
+        <td>Q9</td>
+        <td>GraphQL</td>
+        <td>3/31/21</td>
+        <td>89</td>
+      </tr>
+      <tr>
+        <td>Q10</td>
+        <td>Next.js</td>
+        <td>4/7/21</td>
+        <td>94</td>
+      </tr>
+    </tbody>
+    <tfoot>
+      <tr>
+        <td colSpan={3}>Average</td>
+        <td>90.4</td> 
+      </tr>
+    </tfoot>
+  </table>
+</div>
+
 
 
       <div id="wd-images">
@@ -144,7 +189,6 @@ Your favorite books (in no particular order)
     <label htmlFor="wd-text-fields-last-name">Last name:</label>
     <input type="text" id="wd-text-fields-last-name" placeholder="Doe"
       value="Wonderland" title="The last name" />
-    {/* copy rest of form elements here  */}
   </form>
 </div>
 
