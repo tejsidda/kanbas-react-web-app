1c2e4438f0a58e1cd18b74228314e74ad14e1937
tejsidda <tejsidda34@gmail.com>
2024-10-03 00:09:48 -0500

    a2 css and bootstrap2

--- src/Kanbas/Courses/Navigation.tsx
+++ src/Kanbas/Courses/Navigation.tsx
@@ -1,20 +1,85 @@
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
+
 export default function CoursesNavigation() {
   return (
     <div id="wd-courses-navigation" className="wd list-group fs-5 rounded-0">
-      <Link to="/Kanbas/Courses/1234/Home" id="wd-course-home-link"
-        className="list-group-item active border border-0"> Home </Link>
-      <Link to="/Kanbas/Courses/1234/Modules" id="wd-course-modules-link"
-        className="list-group-item text-danger border border-0"> Modules </Link>
-      <Link to="/Kanbas/Courses/1234/Piazza" id="wd-course-piazza-link"
-        className="list-group-item text-danger border border-0"> Piazza </Link>
-      <Link to="/Kanbas/Courses/1234/Zoom" id="wd-course-zoom-link"
-        className="list-group-item text-danger border border-0"> Zoom </Link>
-      <Link to="/Kanbas/Courses/1234/Assignments" id="wd-course-quizzes-link"
-        className="list-group-item text-danger border border-0"> Assignments </Link>
-      <Link to="/Kanbas/Courses/1234/Quizzes" id="wd-course-assignments-link"
-        className="list-group-item text-danger border border-0"> Quizzes </Link>
-      <Link to="/Kanbas/Courses/1234/People" id="wd-course-people-link"
-        className="list-group-item text-danger border border-0" > People </Link>
+      <NavLink
+        to="/Kanbas/Courses/1234/Home"
+        id="wd-course-home-link"
+        className={({ isActive }) =>
+          isActive
+            ? "list-group-item active border border-0"
+            : "list-group-item text-danger border border-0"
+        }
+      >
+        Home
+      </NavLink>
+      <NavLink
+        to="/Kanbas/Courses/1234/Modules"
+        id="wd-course-modules-link"
+        className={({ isActive }) =>
+          isActive
+            ? "list-group-item active border border-0"
+            : "list-group-item text-danger border border-0"
+        }
+      >
+        Modules
+      </NavLink>
+      <NavLink
+        to="/Kanbas/Courses/1234/Piazza"
+        id="wd-course-piazza-link"
+        className={({ isActive }) =>
+          isActive
+            ? "list-group-item active border border-0"
+            : "list-group-item text-danger border border-0"
+        }
+      >
+        Piazza
+      </NavLink>
+      <NavLink
+        to="/Kanbas/Courses/1234/Zoom"
+        id="wd-course-zoom-link"
+        className={({ isActive }) =>
+          isActive
+            ? "list-group-item active border border-0"
+            : "list-group-item text-danger border border-0"
+        }
+      >
+        Zoom
+      </NavLink>
+      <NavLink
+        to="/Kanbas/Courses/1234/Assignments"
+        id="wd-course-assignments-link"
+        className={({ isActive }) =>
+          isActive
+            ? "list-group-item active border border-0"
+            : "list-group-item text-danger border border-0"
+        }
+      >
+        Assignments
+      </NavLink>
+      <NavLink
+        to="/Kanbas/Courses/1234/Quizzes"
+        id="wd-course-quizzes-link"
+        className={({ isActive }) =>
+          isActive
+            ? "list-group-item active border border-0"
+            : "list-group-item text-danger border border-0"
+        }
+      >
+        Quizzes
+      </NavLink>
+      <NavLink
+        to="/Kanbas/Courses/1234/People"
+        id="wd-course-people-link"
+        className={({ isActive }) =>
+          isActive
+            ? "list-group-item active border border-0"
+            : "list-group-item text-danger border border-0"
+        }
+      >
+        People
+      </NavLink>
     </div>
-);}
+  );
+}
--- src/Kanbas/Navigation.tsx
+++ src/Kanbas/Navigation.tsx
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import { AiOutlineDashboard } from "react-icons/ai";
 import { IoCalendarOutline } from "react-icons/io5";
 import { LiaBookSolid, LiaCogSolid } from "react-icons/lia";
@@ -21,64 +21,88 @@ export default function KanbasNavigation() {
         <img src="/images/NEU.png" width="75px" />
       </a>
       <br />
-      <Link
+      <NavLink
         to="/Kanbas/Account"
         id="wd-account-link"
-        className="list-group-item text-center border-0 bg-black text-white"
+        className={({ isActive }) =>
+          isActive
+            ? "list-group-item text-black bg-white text-center border-0"
+            : "list-group-item text-white bg-black text-center border-0"
+        }
       >
         <FaRegCircleUser className="fs-1 text text-white" />
         <br />
         Account
-      </Link>
+      </NavLink>
 
-      <Link
+      <NavLink
         to="/Kanbas/Dashboard"
         id="wd-dashboard-link"
-        className="list-group-item text-center border-0 bg-black text-white"
+        className={({ isActive }) =>
+          isActive
+            ? "list-group-item text-black bg-white text-center border-0"
+            : "list-group-item text-white bg-black text-center border-0"
+        }
       >
         <AiOutlineDashboard className="fs-1 text-danger" />
         Dashboard
-      </Link>
+      </NavLink>
 
-      <Link
+      <NavLink
         to="/Kanbas/Courses"
         id="wd-course-link"
-        className="list-group-item text-white bg-black text-center border-0"
+        className={({ isActive }) =>
+          isActive
+            ? "list-group-item text-black bg-white text-center border-0"
+            : "list-group-item text-white bg-black text-center border-0"
+        }
       >
         <LiaBookSolid className="fs-1 text-danger" />
         <br />
         Courses
-      </Link>
+      </NavLink>
 
-      <Link
+      <NavLink
         to="/Kanbas/Calendar"
         id="wd-calendar-link"
-        className="list-group-item text-white bg-black text-center border-0"
+        className={({ isActive }) =>
+          isActive
+            ? "list-group-item text-black bg-white text-center border-0"
+            : "list-group-item text-white bg-black text-center border-0"
+        }
       >
         <IoCalendarOutline className="fs-1 text-danger" />
         <br />
         Calendar
-      </Link>
+      </NavLink>
 
-      <Link
+      <NavLink
         to="/Kanbas/Inbox"
         id="wd-inbox-link"
-        className="list-group-item text-white bg-black text-center border-0"
+        className={({ isActive }) =>
+          isActive
+            ? "list-group-item text-black bg-white text-center border-0"
+            : "list-group-item text-white bg-black text-center border-0"
+        }
       >
         <FaInbox className="fs-1 text-danger" />
         <br />
         Inbox
-      </Link>
+      </NavLink>
 
-      <Link
+      <NavLink
         to="/Labs"
         id="wd-labs-link"
-        className="list-group-item text-white bg-black text-center border-0"
+        className={({ isActive }) =>
+          isActive
+            ? "list-group-item text-black bg-white text-center border-0"
+            : "list-group-item text-white bg-black text-center border-0"
+        }
       >
         <FaFlask className="fs-1 text-danger" />
         <br />
         Labs
-      </Link>
+      </NavLink>
     </div>
   );
 }
