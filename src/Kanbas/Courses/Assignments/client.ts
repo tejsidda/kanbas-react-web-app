645ebf58c84d9aa80f6e454cde823951118f3999
tejsidda <tejsidda34@gmail.com>
2024-12-09 17:30:01 -0500

    a6

--- src/Kanbas/Courses/Assignments/client.ts
+++ src/Kanbas/Courses/Assignments/client.ts
@@ -1,23 +1,23 @@
 import axios from "axios";
 const REMOTE_SERVER = process.env.REACT_APP_REMOTE_SERVER;
 const ASSIGNMENTS_API = `${REMOTE_SERVER}/api/assignments`;
-
+const axiosWithCredentials = axios.create({ withCredentials: true });
 export const deleteAssignment = async (id: string) => {
-  const { data } = await axios.delete(`${ASSIGNMENTS_API}/${id}`);
+  const { data } = await axiosWithCredentials.delete(`${ASSIGNMENTS_API}/${id}`);
   return data;
 };
 
 export const updateAssignment = async (assignment: any) => {
-  const { data } = await axios.put(`${ASSIGNMENTS_API}/${assignment._id}`, assignment);
+  const { data } = await axiosWithCredentials.put(`${ASSIGNMENTS_API}/${assignment._id}`, assignment);
   return data;
 };
 export const fetchAllAssignments = async (id: string) => {
-  const { data } = await axios.get(`${ASSIGNMENTS_API}/${id}`);
+  const { data } = await axiosWithCredentials.get(`${ASSIGNMENTS_API}/${id}`);
   return data;
 };
 
 export const createAssignment = async (courseId: string, assignment: any) => {
-  const response = await axios.post(
+  const response = await axiosWithCredentials.post(
     `${ASSIGNMENTS_API}/${courseId}/modules`,
     assignment
   );
